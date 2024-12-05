79f8399725744789d8372a1568b845d25558c480
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2024-12-05 18:43:12 +0100

    More mocking of bike routes, with pydantic models

--- api/db/db.py
+++ api/db/db.py
@@ -1,21 +1,51 @@
 # Fake db used to mock project structure
+from datetime import datetime
+from models.models import Bike
+from models.models import City
+
+current_time = datetime.now()
+mocked_bikes = [    
+    Bike(id=1, battery_level=100, metadata="So cool and red", position=[14.2089, 15.1092],
+         city=City(id=1, city_name="Malmö", country_code="SWE", c_location="pos",
+        created_at=current_time, updated_at=current_time),
+         status="Available", created_at=current_time, updated_at=current_time),
+    Bike(id=2, battery_level=12, metadata="Sad and blue", position=[11.2089, 14.1092],
+         city=City(id=2, city_name="Göteborg", country_code="SWE", c_location="pos",
+        created_at=current_time, updated_at=current_time),
+         status="Under maintenance", created_at=current_time, updated_at=current_time),
+]
 
 
-def get_all_bikes():
-    return {"bikes": [{"bike_id": 1}, {"bike_id": 2}]}
+def get_all_bikes() -> list[Bike]:
+    return mocked_bikes
 
 
-def get_bike(bike_id: int):
-    return {"bike_id": 1, "bike_status": "ok"}
+def get_bike(bike_id: int) -> Bike:
+    for bike in mocked_bikes:
+        if bike.id == bike_id:
+            return bike
+    return None
 
 
 def add_bike(bike):
-    return bike
+    try:
+        mocked_bikes.update({bike})
+        return bike
+    except:
+        return "Ruh roh!"
 
 
 def remove_bike(bike_id: int):
-    return ""
+    for index, bike in enumerate(mocked_bikes):
+        if bike.id == bike_id:
+            mocked_bikes.pop(index)
+            return "Bike removed"
+    raise KeyError
 
 
-def update_bike(bike):
-    return bike
+def update_bike(bike_data: Bike):
+    for bike in mocked_bikes:
+        if bike.id == bike_data.id:
+            bike = bike_data # Maybe some checks if fields are empty
+            return bike
+    raise KeyError
--- api/routes/bikes.py
+++ api/routes/bikes.py
@@ -1,15 +1,9 @@
+from pydantic import Json
 from fastapi import APIRouter, HTTPException, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from db import db
-from pydantic import BaseModel
-
-
-# Change depending on ORM/Database
-class Bike(BaseModel):
-    bike_id: int
-    position: list[int]
-    status: str
-    in_use: bool
-
+from models.models import Bike
 
 router = APIRouter(
     prefix="/v1/bikes",
@@ -20,39 +14,43 @@ router = APIRouter(
 
 # Include geographical filter
 @router.get("/")
-async def get_all_bikes():
+async def get_all_bikes() -> Json[list[Bike]]:
     res = db.get_all_bikes()
-    return res
+    return JSONResponse(jsonable_encoder({'data': res}))
 
 
 @router.get("/{bike_id}")
-async def get_bike(bike_id: int):
+async def get_bike(bike_id: int) -> Json[Bike]:
     res = db.get_bike(bike_id)
-    return res
+    return JSONResponse(jsonable_encoder({'data': [res]}))
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def add_bike(bike: Bike):
+async def add_bike(bike: Bike) -> Json[Bike]:
     try:
-        db.add_bike(bike)
-        return bike
+        new_bike = db.add_bike(bike)
+        return JSONResponse(jsonable_encoder({"message": "New bike added", 'data': [new_bike]}))
     except ValueError:
         raise HTTPException(status_code=500, detail="Internal server error")
 
 
 @router.put("/{bike_id}")
-async def update_bike(bike: Bike):
+async def update_bike(bike: Bike) -> Json[Bike]:
     try:
-        db.update_bike(bike)
-        return bike
+        updated_bike = db.update_bike(bike)
+        return JSONResponse(jsonable_encoder({'data': [updated_bike]}))
     except ValueError:
         raise HTTPException(status_code=500, detail="Internal server error")
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No bike found")
 
 
 @router.delete("/{bike_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def remove_bike(bike_id: int):
+async def remove_bike(bike_id: int) -> None:
     try:
         db.remove_bike(bike_id)
         return
     except ValueError:
         raise HTTPException(status_code=500, detail="Internal server error")
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No bike found")
