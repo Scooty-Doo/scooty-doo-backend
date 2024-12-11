6945c8ddccef15359b8415b1a94985f1029e2785
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2024-12-11 18:30:43 +0100

    CORS fixed

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # from routes.bikes import router as bikes_router
 from routes import bikes, trips, users, zones
@@ -7,6 +8,21 @@ from routes import bikes, trips, users, zones
 app = FastAPI(
     title="Scooty Doo API",
     summary="Everything you need to make a custom app with the Scooty Doo API",
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://localhost",
+    "https://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(bikes.router)
