21c3dfaef74358be4c3672026d4c524a85393714
Martin Nalepa <apelansign@gmail.com>
2024-12-12 13:29:27 +0100

    Correct import

--- api/dependencies.py
+++ api/dependencies.py
@@ -4,7 +4,7 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.database import database, models, repository
+from database import database, models, repository
 
 def get_repository(model: type[models.Base]) -> Callable[[AsyncSession], repository.DatabaseRepository]:
     def func(session: AsyncSession = Depends(database.get_db_session)) -> repository.DatabaseRepository:
