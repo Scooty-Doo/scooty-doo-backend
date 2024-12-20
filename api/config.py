de7c8e8717743c390a22de7a9dbe600eb5c2ec4b
Martin Nalepa <apelansign@gmail.com>
2024-12-20 14:42:15 +0100

    Added default values to config.py, so it doenst crash without .env

--- api/config.py
+++ api/config.py
@@ -29,9 +29,9 @@ class Settings(BaseSettings):
     debug: bool = False
     environment: str = "local"
     database_url: str = "postgresql+asyncpg://user:pass@localhost:5432/sddb"
-    github_client_id: str
-    github_client_secret: str
-    github_redirect_uri: str
+    github_client_id: str = "secretid"
+    github_client_secret: str = "secret"
+    github_redirect_uri: str = "sercret"
 
     class Config:
         """Pydantic model config.
