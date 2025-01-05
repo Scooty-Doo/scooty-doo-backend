072d4acca14ad86c530c13151d28e8db366c3a9e
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2025-01-05 20:55:19 +0100

    Adds test for github oauth

--- api/routes/oauth.py
+++ api/routes/oauth.py
@@ -1,16 +1,17 @@
 """OAuth routes"""
-from typing import Annotated
-from fastapi import APIRouter, HTTPException, Depends
 
 import os
+from typing import Annotated
+
 import jwt
-from api.models.oauth_models import GitHubCode
-from api.oauth.oauth import get_github_access_token, get_github_user
+from fastapi import APIRouter, Depends, HTTPException
+
 from api.db.repository_user import UserRepository as UserRepoClass
-from api.models import db_models
-from api.models.oauth_models import GitHubUserResponse, UserId
 from api.dependencies.repository_factory import get_repository
 from api.exceptions import UserNotFoundException
+from api.models import db_models
+from api.models.oauth_models import GitHubCode, GitHubUserResponse, UserId
+from api.oauth.oauth import get_github_access_token, get_github_user
 
 router = APIRouter(
     prefix="/v1/oauth",
@@ -23,21 +24,15 @@ UserRepository = Annotated[
     Depends(get_repository(db_models.User, repository_class=UserRepoClass)),
 ]
 
+
 @router.post("/github")
-async def login_github(
-    code: GitHubCode,
-    user_repository: UserRepository,):
+async def login_github(code: GitHubCode, user_repository: UserRepository):
     """Login with GitHub"""
     try:
-        print("-----------------------------------\n")
-        print("-----------------------------------\n")
-        print("GITHUBCODE: ", code)
-        print("-----------------------------------\n")
-        print("-----------------------------------\n")
         access_token = await get_github_access_token(code.code)
         github_user_data = await get_github_user(access_token)
         github_user = GitHubUserResponse.model_validate(github_user_data)
-        
+
         try:
             user_id = await user_repository.get_user_id_from_github_login(github_user.login)
             user_id = UserId(id=user_id)
@@ -46,17 +41,8 @@ async def login_github(
             user = await user_repository.create_user(user_create.model_dump())
             user_id = UserId(id=user.id)
 
-        token = jwt.encode({'user_id': user_id.id}, os.getenv("JWT_SECRET"), algorithm="HS256")
-        print("-----------------------------------\n")
-        print("-----------------------------------\n")
-        print("-----------------------------------\n")
-        print("-----------------------------------\n")
-        print("TOKEN: ", token)
-        # print("USER ID: ", user_id)
-        print("-----------------------------------\n")
-        print("-----------------------------------\n")
-        print("-----------------------------------\n")
-        return {'token': token}
+        token = jwt.encode({"user_id": user_id.id}, os.getenv("JWT_SECRET"), algorithm="HS256")
+        return {"token": token}
         # return user_id
     except HTTPException as e:
         raise e
