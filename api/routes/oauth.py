02baefcfc56e82e0dd4cbb38c5dd29912258d444
Martin Nalepa <apelansign@gmail.com>
2025-01-05 16:36:24 +0100

    Routes, models, repo method for logging in user

--- api/routes/oauth.py
+++ api/routes/oauth.py
@@ -1,9 +1,14 @@
 """OAuth routes"""
-
-from fastapi import APIRouter, HTTPException
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, Depends
 
 from api.models.oauth_models import GitHubCode
 from api.oauth.oauth import get_github_access_token, get_github_user
+from api.db.repository_user import UserRepository as UserRepoClass
+from api.models import db_models
+from api.models.oauth_models import GitHubUserResponse, UserId
+from api.dependencies.repository_factory import get_repository
+from api.exceptions import UserNotFoundException
 
 router = APIRouter(
     prefix="/v1/oauth",
@@ -11,14 +16,27 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+UserRepository = Annotated[
+    UserRepoClass,
+    Depends(get_repository(db_models.User, repository_class=UserRepoClass)),
+]
 
 @router.post("/github")
-async def login_github(code: GitHubCode):
+async def login_github(
+    code: GitHubCode,
+    user_repository: UserRepository,):
     """Login with GitHub"""
     try:
         access_token = await get_github_access_token(code.code)
-        user_info = await get_github_user(access_token)
-        print(user_info)
-        return {"token": access_token}
-    except HTTPException as e:
-        return {"error": str(e.detail)}
+        github_user_data = await get_github_user(access_token)
+        github_user = GitHubUserResponse.model_validate(github_user_data)
+        
+        try:
+            user_id = await user_repository.get_user_id_from_github_login(github_user.login)
+        except UserNotFoundException:
+            user = await user_repository.create_user(github_user.to_user_create())
+            user_id = UserId(id=user.id)
+            
+        return user_id
+    except HTTPException:
+        raise 
