c21efba746fd0228b93f171c66423daad2f0d53d
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2025-01-05 16:39:35 +0100

    Adds test for oauth login

---
+++ tests/test_github_oauth.py
@@ -0,0 +1,47 @@
+"""Module for testing github oauth"""
+
+from unittest.mock import AsyncMock
+
+import os
+import pytest
+from httpx import ASGITransport, AsyncClient
+
+from api.db.repository_user import UserRepository
+from api.main import app
+
+from tests.mock_files.objects import fake_user_data
+import jwt
+
+
+class TestOauthRoutes:
+    """Class to test github oauth routes"""
+
+    @pytest.mark.asyncio
+    async def test_oauth_github(self, monkeypatch):
+        """Tests v1/oauth/github route"""
+
+        # Mock github token call
+        mock_access_token = AsyncMock(return_value="awoidhaw90")
+        monkeypatch.setattr("api.routes.oauth.get_github_access_token", mock_access_token)
+        # Mock github user call
+        mock_github_user = AsyncMock(return_value={'login': "HasseBron"})
+        monkeypatch.setattr("api.routes.oauth.get_github_user", mock_github_user)
+
+        # Mock database call
+        mock_get_user = AsyncMock(return_value=fake_user_data)
+        monkeypatch.setattr(UserRepository, "get_user", mock_get_user)
+
+        async with AsyncClient(
+            transport=ASGITransport(app=app), base_url="http://localhost:8000/"
+        ) as ac:
+            response = await ac.post(
+                url="v1/oauth/github",
+                headers= {'content-type': 'application/json'},
+                json={'code': '890oaihdawopdgh'})
+
+        mock_get_user.assert_called_once()
+        assert response.status_code == 200
+        response = response.json()
+        assert response['token']
+        expected_response = jwt.decode(response['token'], os.getenv("JWT_SECRET"), algorithms=["HS256"])
+        assert fake_user_data.id == expected_response['user_id']
