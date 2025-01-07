6ce27bc545464ec5ccbe9fb5dd2ada4371b63a03
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2025-01-07 14:20:06 +0100

    Adds github_login field to admin table

--- api/models/db_models.py
+++ api/models/db_models.py
@@ -216,7 +216,8 @@ class Admin(Base):
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
     full_name: Mapped[str] = mapped_column(Text, nullable=False)
     email: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
-    meta_data: Mapped[dict] = mapped_column(JSONB, nullable=True)
+    github_login: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
+    meta_data: Mapped[dict] = mapped_column(JSONB, nullable=True)    
 
     roles: Mapped[list["AdminRole"]] = relationship(
         secondary="admin_2_admin_roles", back_populates="admins"
