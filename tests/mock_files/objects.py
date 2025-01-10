49823b40cb563344c99b448920f4cbd20221099f
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2025-01-10 01:35:03 +0100

    Adds test for socket emit on start and end trip

--- tests/mock_files/objects.py
+++ tests/mock_files/objects.py
@@ -1,6 +1,6 @@
 import datetime
 
-from api.models.db_models import Bike, User
+from api.models.db_models import Bike, Trip, User
 
 fake_bike_data = [
     Bike(
@@ -29,3 +29,11 @@ fake_bike_data = [
 ]
 
 fake_user_data = User(id=652134919185249719)
+
+fake_trip = Trip(
+    id=12409712904,
+    start_position="POINT(13.05 55.23)",
+    start_time=datetime.datetime(2024, 7, 13, 7, 56, 51, 758246),
+    created_at=datetime.datetime(2024, 7, 13, 7, 56, 51, 758246, tzinfo=datetime.timezone.utc),
+    updated_at=datetime.datetime(2024, 12, 17, 14, 21, 46, 610901, tzinfo=datetime.timezone.utc),
+)
