8da9ead14d9864baa914e79a4048321ffa3b3128
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2025-01-17 13:01:18 +0100

    Adds test for patch /me

--- tests/routes/test_me.py
+++ tests/routes/test_me.py
@@ -1,6 +1,7 @@
 """Module for testing trip routes"""
 
-from unittest.mock import AsyncMock, Mock
+import json
+from unittest.mock import AsyncMock
 
 import pytest
 from fastapi.security.oauth2 import SecurityScopes
@@ -9,7 +10,7 @@ from httpx import ASGITransport, AsyncClient
 from api.db.repository_user import UserRepository
 from api.main import app
 from api.routes.me import security_check
-from tests.mock_files.objects import fake_me_data
+from tests.mock_files.objects import fake_me_data, fake_updated_me
 from tests.utils import get_fake_json_data
 
 
@@ -22,7 +23,7 @@ class TestMe:
 
     @pytest.mark.asyncio
     async def test_get_me(self, monkeypatch):
-        """Tests get trips route"""
+        """Tests get me route"""
 
         app.dependency_overrides[security_check] = self.mock_security_check
 
@@ -36,3 +37,38 @@ class TestMe:
 
         assert response.status_code == 200
         assert response.json() == get_fake_json_data("me")
+
+    @pytest.mark.asyncio
+    async def test_patch_me(self, monkeypatch):
+        """Tests patch me route"""
+
+        app.dependency_overrides[security_check] = self.mock_security_check
+
+        mock_data = {
+            "full_name": "Stringy Boy",
+            "email": "user@example.com",
+            "github_login": "K",
+            "use_prepay": True,
+            "meta_data": {},
+        }
+
+        # Mock away update
+        mock_update = AsyncMock(return_value=fake_updated_me)
+        monkeypatch.setattr(UserRepository, "update_user", mock_update)
+
+        async with AsyncClient(
+            transport=ASGITransport(app=app), base_url="http://localhost:8000/"
+        ) as ac:
+            response = await ac.patch("v1/me/", data=json.dumps(mock_data))
+
+        assert response.status_code == 200
+        mock_update.assert_awaited_once_with(
+            1201279949800724,
+            {
+                "full_name": "Stringy Boy",
+                "email": "user@example.com",
+                "github_login": "K",
+                "use_prepay": True,
+                "meta_data": {},
+            },
+        )
